3259739ecc7ce8e3926a89716a104ec5d0cbab06
Danillo Coelho <danillo.06@hotmail.com>
2025-09-24 11:10:57 -0300

    fix: ajustar eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,9 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "@next/next/no-img-element": "off", // 👈 desliga os avisos de <img>
+    },
   },
 ];
 
